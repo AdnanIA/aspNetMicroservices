cb4511dad42973e1a5908c0facace656d55b8a9a
adnanshaukat1994 <Adnan.Shaukat@abacus-global.com>
2022-02-14 10:32:04 +0500

    latest changes

---
+++ aspNet-RunMicroServices/Catalog.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>>GetProducts();
+        Task<Product> GetProduct(string id);
+    }
+}
